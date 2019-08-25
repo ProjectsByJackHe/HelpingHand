9e49cbea1c60a3edee5ad3f9c1cc8b06375846a1
Darryl Wu <darryl.wu301@gmail.com>
2019-08-25 03:10:05 -0400

    Small changes and READ ME

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 let longitude;
 let latitude;
-let JohnDoe = "<h6><img src = \"Assets/pic3.png\"></h6><h6>John Doe<br>Skills: Calculus <br>Tutor - $20/hr<br>Tel: 617 294 7592<br>Email: jdoe@gmail.com</h6>"
-let JohnSmith = "<h6><img src = \"Assets/pic4.png\"></h6><h6>John Smith<br>Skills: Garden Engineer - $50/hr<br>Tel: 465 897 1209<br>Email: jsmith@gardenhoe.com</h6>"
-let NickFury = "<h6><img src = \"Assets/pic6.png\"></h6><h6>Nick Fury<br>Skills: Plumber - $22/hr<br>Tel: 325 224 7894<br>Email: nfury@live.com</h6>"
-let BobJones = "<h6><img src = \"Assets/pic8.png\" height=\"90\" width=\"90\"></h6><h6>Bob Jones<br>Skills: Physics <br>Tutor - $18/hr<br>Tel: 894 238 9230<br>Email: bojo@yahoo.com</h6>"
-let KathyTran = "<h6><img src = \"Assets/pic2.png\"></h6><h6>Kathy Tran<br>Skills: Chemistry <br>Tutor - $25/hr<br>Tel: 342 812 7538<br>Email: Katran@gmail.com</h6>"
-let AlexNguyen = "<h6><img src = \"Assets/pic6.png\"></h6><h6>Alex Nguyen<br>Skills: Linear Algebra <br>Tutor - $20/hr<br>Tel: 894 237 2389<br>Email: alexnguyen@hotmail.com</h6>"
-let OldMcdonald = "<h6><img src = \"Assets/pic7.png\"></h6><h6>Old McDonald<br>Skills: Electrican - $40/hr<br>Tel: 216 395 9880<br>Email: omcdonald@gmail.com</h6>"
-let KevinMatthew = "<h6><img src = \"Assets/pic8.png\" height=\"90\" width=\"90\"></h6><h6>Kevin Matthew<br>Skills: Painter - $25/hr<br>Tel: 932 752 2918<br>Email: kevmatt@live.com</h6>"
-let LukeMatthew = "<h6><img src = \"Assets/pic3.png\"></h6><h6>Luke Matthew<br>Skills: Pool Cleaner - $18/hr<br>Tel: 912 834 7313<br>Email: lukem@yahoo.com</h6>"
-let GiJoe = "<h6><img src = \"Assets/pic4.png\"></h6><h6>GI Joe<br>Skills: Landscaper - $30/hr<br>Tel: 429 149 7538<br>Email: gijoe@outlook.com</h6>"
-let HeroldWong = "<h6><img src = \"Assets/pic6.png\"></h6><h6>Herold Wong<br>Skills: Rooftop Maintainance - $35/hr<br>Tel: 842 901 9841<br>Email: hwong@gmail.com</h6>"
-let TylerZhang = "<h6><img src = \"Assets/pic8.png\" height=\"90\" width=\"90\"></h6><h6>Tyler Zhang<br>Skills: Furtniture Installer - $15/hr<br>Tel: 941 851 9159<br>Email: tyzhang@outlook.com</h6>"
+let JohnDoe = "<h6><img src = \"Assets/pic3.png\"></h6><h6>John Doe<br>Skills: Calculus <br>Tutor - $20/hr<br>Tel: 617 294 7592<br>Email: jdoe@gmail.com<br><br><a href=\"ViewPendingService.html\"><button>Request Service</button></a></h6>"
+let JohnSmith = "<h6><img src = \"Assets/pic4.png\"></h6><h6>John Smith<br>Skills: Garden Engineer - $50/hr<br>Tel: 465 897 1209<br>Email: jsmith@gardenhoe.com<br><br><a href=\"ViewPendingService.html\"><button>Request Service</button></a></h6>"
+let NickFury = "<h6><img src = \"Assets/pic6.png\"></h6><h6>Nick Fury<br>Skills: Plumber - $22/hr<br>Tel: 325 224 7894<br>Email: nfury@live.com<br><br><a href=\"ViewPendingService.html\"><button>Request Service</button></a></h6>"
+let BobJones = "<h6><img src = \"Assets/pic8.png\" height=\"90\" width=\"90\"></h6><h6>Bob Jones<br>Skills: Physics <br>Tutor - $18/hr<br>Tel: 894 238 9230<br>Email: bojo@yahoo.com<br><br><a href=\"ViewPendingService.html\"><button>Request Service</button></a></h6>"
+let KathyTran = "<h6><img src = \"Assets/pic2.png\"></h6><h6>Kathy Tran<br>Skills: Chemistry <br>Tutor - $25/hr<br>Tel: 342 812 7538<br>Email: Katran@gmail.com<br><br><a href=\"ViewPendingService.html\"><button>Request Service</button></a></h6>"
+let AlexNguyen = "<h6><img src = \"Assets/pic6.png\"></h6><h6>Alex Nguyen<br>Skills: Linear Algebra <br>Tutor - $20/hr<br>Tel: 894 237 2389<br>Email: alexnguyen@hotmail.com<br><br><a href=\"ViewPendingService.html\"><button>Request Service</button></a></h6>"
+let OldMcdonald = "<h6><img src = \"Assets/pic7.png\"></h6><h6>Old McDonald<br>Skills: Electrican - $40/hr<br>Tel: 216 395 9880<br>Email: omcdonald@gmail.com<br><br><a href=\"ViewPendingService.html\"><button>Request Service</button></a></h6>"
+let KevinMatthew = "<h6><img src = \"Assets/pic8.png\" height=\"90\" width=\"90\"></h6><h6>Kevin Matthew<br>Skills: Painter - $25/hr<br>Tel: 932 752 2918<br>Email: kevmatt@live.com<br><br><a href=\"ViewPendingService.html\"><button>Request Service</button></a></h6>"
+let LukeMatthew = "<h6><img src = \"Assets/pic3.png\"></h6><h6>Luke Matthew<br>Skills: Pool Cleaner - $18/hr<br>Tel: 912 834 7313<br>Email: lukem@yahoo.com<br><br><a href=\"ViewPendingService.html\"><button>Request Service</button></a></h6>"
+let GiJoe = "<h6><img src = \"Assets/pic4.png\"></h6><h6>GI Joe<br>Skills: Landscaper - $30/hr<br>Tel: 429 149 7538<br>Email: gijoe@outlook.com<br><br><a href=\"ViewPendingService.html\"><button>Request Service</button></a></h6>"
+let HeroldWong = "<h6><img src = \"Assets/pic6.png\"></h6><h6>Herold Wong<br>Skills: Rooftop Maintainance - $35/hr<br>Tel: 842 901 9841<br>Email: hwong@gmail.com<br><br><a href=\"ViewPendingService.html\"><button>Request Service</button></a></h6>"
+let TylerZhang = "<h6><img src = \"Assets/pic8.png\" height=\"90\" width=\"90\"></h6><h6>Tyler Zhang<br>Skills: Furtniture Installer - $15/hr<br>Tel: 941 851 9159<br>Email: tyzhang@outlook.com<br><br><a href=\"ViewPendingService.html\"><button>Request Service</button></a></h6>"
 
 
 
