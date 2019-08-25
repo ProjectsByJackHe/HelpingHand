f2b4ddb08120539d1f479496f4b5aa6b3b33a4ff
Jack He <jack.he@gameofapps.org>
2019-08-24 21:29:03 -0400

    added firebase library

--- login.js
+++ login.js
@@ -1,16 +1,25 @@
+
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-     
+    window.alert("You're signed in!") 
   } else {
     // No user is signed in.
-      
+    window.alert("You're not signed in!")
   }
 });
 
 
 function login(){
     //window.alert("yessir")
-    let email = document.getElementById("textBar").value 
-    let password = document.getElementById("passwordBar").value 
+    let userEmail = document.getElementById("textBar").value 
+    let userPassword = document.getElementById("passwordBar").value 
+    document.alert(userEmail)
+firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+    window.alert("error: " + errorCode + " " + errorMessage)
+});
 }
