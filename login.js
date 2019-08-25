d4b156c20ad7f1db5133976ae66c8cd849d5e821
Jack He <jack.he@gameofapps.org>
2019-08-25 00:55:00 -0400

    got auth to work

--- login.js
+++ login.js
@@ -1,4 +1,18 @@
 
+
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDhMsBcaAksOvMUKm85IuOFqYWHgy5cuhc",
+    authDomain: "hackthe6ix-4ab0a.firebaseapp.com",
+    databaseURL: "https://hackthe6ix-4ab0a.firebaseio.com",
+    projectId: "hackthe6ix-4ab0a",
+    storageBucket: "",
+    messagingSenderId: "481485805668",
+    appId: "1:481485805668:web:ef2a73802205e995"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
@@ -14,12 +28,11 @@ function login(){
     //window.alert("yessir")
     let userEmail = document.getElementById("textBar").value 
     let userPassword = document.getElementById("passwordBar").value 
-    window.alert(userEmail)
 firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
   // ...
-    window.alert("error: " + errorCode + " " + errorMessage)
+window.alert("error: " + " " + errorMessage)
 });
 }
